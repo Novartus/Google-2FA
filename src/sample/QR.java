5ff5a0792d55005bb902fe30ba5305fdec767c10
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-16 03:55:23 +0530

    2FA Console Magic Code Added

--- src/sample/QR.java
+++ src/sample/QR.java
@@ -1,63 +1,103 @@
 package sample;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.EnumMap;
-import java.util.Map;
-import javax.imageio.ImageIO;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+
+import com.google.zxing.common.ByteMatrix;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+import Tools.TOTP;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QR {
-    public  void qr_generate() {
-        String myCodeText = "https://google.com/";
-        String filePath = "/media/hudaniabhee/Cooking/Java/App_FX/QR-Code/QR.png";
-        int size = 250;
-        String fileType = "png";
-        File myFile = new File(filePath);
-        try {
 
-            Map<EncodeHintType, Object> hintMap = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
-            hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+    public static String getRandomSecretKey() {
+        SecureRandom secureRandom = new SecureRandom(); // Cryptographically Random Number Generator (RNG)
+        byte[] bytes = new byte[20];
+        secureRandom.nextBytes(bytes);
+        Base32 base32 = new Base32();
+        String SecretKey = base32.encodeToString(bytes);
+        // make the secret key more human-readable by lower-casing and
+        // inserting spaces between each group of 4 characters
+        return SecretKey.toLowerCase().replaceAll("(.{4})(?=.{4})", "$1 ");
+    }
 
-            // Now with zxing version 3.2.1 you could change border size (white border size to just 1)
-            hintMap.put(EncodeHintType.MARGIN, 1); /* default = 4 */
-            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-
-            QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText, BarcodeFormat.QR_CODE, size,
-                    size, hintMap);
-            int QRWidth = byteMatrix.getWidth();
-            BufferedImage image = new BufferedImage(QRWidth, QRWidth,
-                    BufferedImage.TYPE_INT_RGB);
-            image.createGraphics();
-
-            Graphics2D graphics = (Graphics2D) image.getGraphics();
-            graphics.setColor(Color.WHITE);
-            graphics.fillRect(0, 0, QRWidth, QRWidth);
-            graphics.setColor(Color.BLACK);
-
-            for (int i = 0; i < QRWidth; i++) {
-                for (int j = 0; j < QRWidth; j++) {
-                    if (byteMatrix.get(i, j)) {
-                        graphics.fillRect(i, j, 1, 1);
-                    }
-                }
-            }
-            ImageIO.write(image, fileType, myFile);
-        } catch (WriterException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    /**
+     * @param SecretKey Base32 encoded secret key (may have optional whitespace)
+     * @param userid The user's account name. e.g. an email address or a username
+     * @param issuer The organization managing this account
+     * @see https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+     **/
+    public static String getGoogleAuthenticatorBarCode(String SecretKey, String userid, String issuer) {
+        String normalizedBase32Key = SecretKey.replace(" ", "").toUpperCase();
+        try { //Encoding URL
+            return "otpauth://totp/"
+                    + URLEncoder.encode(issuer + ":" + userid, "UTF-8").replace("+", "%20")
+                    + "?secret=" + URLEncoder.encode(normalizedBase32Key, "UTF-8").replace("+", "%20")
+                    + "&issuer=" + URLEncoder.encode(issuer, "UTF-8").replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
         }
-        System.out.println("\n\n You have successfully created QR Code.");
+    }
+
+    public static void createQRCode(String barCodeData, String filePath, int height, int width)
+            throws WriterException, IOException {
+        ByteMatrix matrix = new MultiFormatWriter().encode(barCodeData, BarcodeFormat.QR_CODE,
+                width, height);
+        try (FileOutputStream out = new FileOutputStream(filePath)) {
+            MatrixToImageWriter.writeToStream(matrix, "png", out);
+        }
+    }
+
+    public static String TOTPCode(String SecretKey) {
+        String normalizedBase32Key = SecretKey.replace(" ", "").toUpperCase();
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(normalizedBase32Key);
+        String hexKey = Hex.encodeHexString(bytes);
+        long Time = (System.currentTimeMillis() / 1000) / 30;
+        String HexTime = Long.toHexString(Time);
+        return TOTP.generateTOTP(hexKey, HexTime, "6");
+    }
+
+    public static void main(String[] args) throws Exception {
+        // required for generating the PNG file on a server with no graphics hardware
+        System.setProperty("java.awt.headless", "true");
+
+        String SecretKey = getRandomSecretKey();
+        String BarCode = getGoogleAuthenticatorBarCode(SecretKey, "email@example.com", "github.com/Novartus");
+        String Tmp_Dir = System.getProperty("java.io.tmpdir");
+        if (!Tmp_Dir.endsWith(File.separator)) {
+            Tmp_Dir += File.separator;
+        }
+        String QR_Path = Tmp_Dir + "2FA-QR-Code.png";
+        createQRCode(BarCode, QR_Path, 400, 400);
+
+        System.out.println("\n Configure the Google Authenticator App by scanning the following QR code image:\n");
+        System.out.println(QR_Path + "\n");
+        System.out.println("or by manually entering the secret key:\n");
+        System.out.println(SecretKey + "\n");
+        System.out.println("Then verify that the 6 digit codes generated by Google Authenticator\n"
+                            + "are synchronized with the following :\n");
+
+        String lastCode = null;
+        while (true) {
+            String code = TOTPCode(SecretKey);
+            if (!code.equals(lastCode)) {
+                System.out.println(code);
+            }
+            lastCode = code;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {};
+        }
     }
 }
